31e5651a006741e15f12007aa16399fa2c552fcf
Stephen Smith <steve@rzw.ca>
2018-08-27 17:41:22 -0300

    Add initial dataset

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    instructions = models.TextField()
+    ingredients = models.ManyToManyField('Ingredient',blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def is_vegetarian(self):
+        for recipe_ingredient in self.ingredients.all():
+            if recipe_ingredient.isMeat:
+                return False
+
+        return True
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    isMeat = models.BooleanField()
+    recipes = models.ManyToManyField(Recipe,blank=True)
+
+    def __str__(self):
+        return self.name
